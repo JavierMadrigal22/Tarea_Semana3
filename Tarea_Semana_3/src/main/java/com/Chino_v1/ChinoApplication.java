014dcb944760c5f1190f7114bcd7ba297b19c3d5
JavierMadrigal22 <84678572+JavierMadrigal22@users.noreply.github.com>
2023-10-05 11:38:16 -0600

    Commit de Tarea semana 3
    A continuacion se sube todo el codigo de la tienda "El Chino"

---
+++ Tarea_Semana_3/src/main/java/com/Chino_v1/ChinoApplication.java
@@ -0,0 +1,13 @@
+package com.Chino_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinoApplication.class, args);
+	}
+
+}
